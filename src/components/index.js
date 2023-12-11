2564b40fac24f3252d810d9baf6b3792f7045acc
Gaush1 <gaush5u786@gmail.com>
2023-12-11 19:01:32 +0530

    component added

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,19 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button';
+import Input from './Input';
+import Select from "./Select";
+import PostCard from './PostCard'
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard
 }
